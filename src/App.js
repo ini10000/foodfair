fca88baf3f14e1f54870e087561a8fb782347a3f
ini10000 <iniobong10000@gmail.com>
2022-10-02 22:20:15 +0100

    fixed input size

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Hero />
       <Works />
       <Categories />
-
       <Recommendation />
       <Footer />
     </div>
--- src/views/Hero.js
+++ src/views/Hero.js
@@ -11,9 +11,9 @@ function Hero() {
         <span className="text-[#FF8A00]"> FOOD VENDORS </span>&
         <span className="text-[#FF8A00]"> FOODIES</span>
       </p>
-      <div className="mx-auto flex mt-16 max-w-screen w-[70%] md:w-[38.125rem] h-[4rem] items-center justify-between mb-32">
+      <div className="mx-auto flex mt-16 max-w-screen w-[75%] md:w-[38.125rem] h-[4rem] items-center justify-between mb-32">
         <input
-          className="w-[28.125rem] flex items-center rounded-2xl px-4 py-[1.25rem] h-full border border-[#000000]/0.6 bg-[#FDF9F5]  bg-[url('/src/assets/images/location.svg')]  bg-auto bg-left bg-origin-content bg-no-repeat indent-7 ease-in-out duration-300 focus:bg-none focus:indent-0 cursor-text"
+          className="w-[28.125rem] flex items-center rounded-2xl px-4 py-[1.25rem] h-[3.0625rem]  border border-[#000000]/0.6 bg-[#FDF9F5]  bg-[url('/src/assets/images/location.svg')]  bg-auto bg-left bg-origin-content bg-no-repeat indent-7 ease-in-out duration-300 focus:bg-none focus:indent-0 cursor-text"
           placeholder="Enter Location"
         ></input>
         <Button text="Search" />
